199856a75ec13ceb1af82d8bb1adddeb00f11b4f
broden-horizon <rlghd1698@naver.com>
2023-01-27 18:10:18 +0900

    init

---
+++ src/main/java/com/godsang/anytimedelivery/AnytimedeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.godsang.anytimedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnytimedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnytimedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godsang/anytimedelivery/AnytimedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godsang.anytimedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnytimedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
