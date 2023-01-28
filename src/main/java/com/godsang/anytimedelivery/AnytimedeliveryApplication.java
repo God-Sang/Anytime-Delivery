2bceb59938bbcc124bc745aa224e5a2526898739
NAM GIBEOM <rlqja0523@naver.com>
2023-01-29 01:04:17 +0900

    fix(general): auditing 적용

--- src/main/java/com/godsang/anytimedelivery/AnytimedeliveryApplication.java
+++ src/main/java/com/godsang/anytimedelivery/AnytimedeliveryApplication.java
@@ -2,8 +2,10 @@ package com.godsang.anytimedelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AnytimedeliveryApplication {
 
 	public static void main(String[] args) {
