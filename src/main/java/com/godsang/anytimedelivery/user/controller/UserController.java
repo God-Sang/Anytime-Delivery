354d40685dd24e2454bb9f7994f28890d83dbd62
NAM GIBEOM <rlqja0523@naver.com>
2023-01-29 00:48:41 +0900

    feat(join): 회원가입 #1

---
+++ src/main/java/com/godsang/anytimedelivery/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.godsang.anytimedelivery.user.controller;
+
+import com.godsang.anytimedelivery.user.dto.UserDto;
+import com.godsang.anytimedelivery.user.entity.User;
+import com.godsang.anytimedelivery.user.mapper.UserMapper;
+import com.godsang.anytimedelivery.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void joinUser(@RequestBody UserDto userDto) {
+        User user = userMapper.dtoToUser(userDto);
+        userService.createUser(user, userDto.isCustomer());
+    }
+}
