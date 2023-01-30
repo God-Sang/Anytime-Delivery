2d7090b0d0ec77668266b858fff1d2f335701ba8
NAM GIBEOM <rlqja0523@naver.com>
2023-01-30 16:07:16 +0900

    feat(general): redis session, cache 설정

---
+++ src/main/java/com/godsang/anytimedelivery/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.godsang.anytimedelivery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.session.port}")
+    private int sessionPort;
+    @Value("${spring.redis.cache.port}")
+    private int cachePort;
+
+    @Bean
+    public RedisConnectionFactory redisSessionConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(sessionPort);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisConnectionFactory redisCacheConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(cachePort);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
