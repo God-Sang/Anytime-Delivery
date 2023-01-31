fa384a4d3c1c3b2e00990ec7b14f621b2cc27cb8
broden-horizon <rlghd1698@naver.com>
2023-01-31 18:16:56 +0900

    feat(general) : Redis Cache Manger 설정

--- src/main/java/com/godsang/anytimedelivery/config/RedisConfig.java
+++ src/main/java/com/godsang/anytimedelivery/config/RedisConfig.java
@@ -1,17 +1,25 @@
 package com.godsang.anytimedelivery.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
+@EnableCaching
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String host;
@@ -29,6 +37,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Qualifier("redisConnectionFactoryForCache")
     public RedisConnectionFactory redisCacheConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
@@ -48,6 +57,26 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(serializer);
         redisTemplate.setEnableTransactionSupport(true);
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+            .defaultCacheConfig()
+            .disableCachingNullValues()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair
+                    .fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair
+                    .fromSerializer(new GenericJackson2JsonRedisSerializer())
+            )
+            .entryTtl(Duration.ofDays(3L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(redisCacheConnectionFactory())
+            .cacheDefaults(redisCacheConfiguration)
+            .build();
     }
 
     @Bean
