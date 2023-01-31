b60f61b85bdb6a40b4094deda596c955280a8d4f
NAM GIBEOM <rlqja0523@naver.com>
2023-01-31 18:01:01 +0900

    feat(login): security 설정 추가 #2

--- src/main/java/com/godsang/anytimedelivery/config/SecurityConfig.java
+++ src/main/java/com/godsang/anytimedelivery/config/SecurityConfig.java
@@ -1,18 +1,25 @@
 package com.godsang.anytimedelivery.config;
 
+import com.godsang.anytimedelivery.auth.handler.*;
+import com.godsang.anytimedelivery.user.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
+    private final String ADMIN = Role.ROLE_ADMIN.getName();
+    private final String CUSTOMER = Role.ROLE_CUSTOMER.getName();
+    private final String OWNER = Role.ROLE_OWNER.getName();
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -20,15 +27,35 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .cors(Customizer.withDefaults()) // cors
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 생성 x
+                .formLogin() // 로그인 설정
+                .loginProcessingUrl("/users/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .successHandler(new CustomAuthenticationSuccessHandler())
+                .failureHandler(new CustomAuthenticationFailureHandler())
                 .and()
-                .formLogin().disable()
-                .httpBasic().disable() // jwt 쓸거면 비활성화
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
-                );
+                .logout() // 로그아웃 설정
+                .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
+                .logoutSuccessHandler(new CustomLogoutSuccessHandler())
+                .invalidateHttpSession(true) // 세션 초기화
+                .and()
+                .exceptionHandling() // 예외 처리 handler
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .authorizeHttpRequests() // TODO 권한 path 분기
+                .antMatchers(HttpMethod.POST, "/customer/**").hasRole(CUSTOMER)
+                .antMatchers(HttpMethod.GET, "/customer/**").hasRole(CUSTOMER)
+                .antMatchers(HttpMethod.PATCH, "/customer/**").hasRole(CUSTOMER)
+                .antMatchers(HttpMethod.DELETE, "/customer/**").hasRole(CUSTOMER)
+                .antMatchers(HttpMethod.POST, "/owner/**").hasRole(OWNER)
+                .antMatchers(HttpMethod.GET, "/owner/**").hasRole(OWNER)
+                .antMatchers(HttpMethod.PATCH, "/owner/**").hasRole(OWNER)
+                .antMatchers(HttpMethod.DELETE, "/owner/**").hasRole(OWNER)
+                .anyRequest().permitAll();
         return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
