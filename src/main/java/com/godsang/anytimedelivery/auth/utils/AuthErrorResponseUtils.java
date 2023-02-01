8e547a47ecf821ca974b6a1093594f6dd55e631a
NAM GIBEOM <rlqja0523@naver.com>
2023-02-02 00:41:58 +0900

    feat(general): advice 추가 closes #22

--- src/main/java/com/godsang/anytimedelivery/auth/utils/AuthErrorResponseUtils.java
+++ src/main/java/com/godsang/anytimedelivery/auth/utils/AuthErrorResponseUtils.java
@@ -1,5 +1,6 @@
 package com.godsang.anytimedelivery.auth.utils;
 
+import com.godsang.anytimedelivery.common.Exception.ErrorResponse;
 import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,6 +17,6 @@ public class AuthErrorResponseUtils {
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(status.value());
-        response.getWriter().write(gson.toJson(message)); // TODO 공통 response로 wrapping
+        response.getWriter().write(gson.toJson(ErrorResponse.of(status, message)));
     }
 }
