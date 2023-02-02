3f41e306d09a61f246210a9aa3e30e472a18a756
broden-horizon <rlghd1698@naver.com>
2023-02-02 15:02:08 +0900

    feat(store): 카테고리ID로 스토어 검색하는 기능
    fixes: #5

--- src/main/java/com/godsang/anytimedelivery/store/repository/StoreRepository.java
+++ src/main/java/com/godsang/anytimedelivery/store/repository/StoreRepository.java
@@ -1,7 +1,13 @@
 package com.godsang.anytimedelivery.store.repository;
 
 import com.godsang.anytimedelivery.store.entity.Store;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+  @Query("select s from Store s join s.categoryStores cs join cs.category c where c.categoryId=?1")
+  List<Store> findStoresByCategory(Long categoryId, Pageable pageable);
 }
