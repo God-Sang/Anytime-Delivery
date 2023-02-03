9a99a9e73cdd2d3bcfeea2d0341721dbcc472af7
broden-horizon <rlghd1698@naver.com>
2023-02-03 16:13:40 +0900

    refactor(general): @EnableJpaAuditing을 따로 클래스를 만들어서 옮김
    @WebMvcTest 사용시, 기본적으로 XXXAplication을 로딩하는데, 그때 @EnableJpaAuditing이 붙어있으면, Jpa 관련 빈을 찾으려고 함. 근데 WebMvc는 Controller 계층 관련 Bean만 등록하기 때문에 에러가 발생함.

--- src/main/java/com/godsang/anytimedelivery/AnytimedeliveryApplication.java
+++ src/main/java/com/godsang/anytimedelivery/AnytimedeliveryApplication.java
@@ -2,10 +2,8 @@ package com.godsang.anytimedelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class AnytimedeliveryApplication {
   public static void main(String[] args) {
     SpringApplication.run(AnytimedeliveryApplication.class, args);
