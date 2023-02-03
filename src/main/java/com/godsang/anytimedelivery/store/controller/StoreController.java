6733b99a71313208b94df2d586fa338b508f24a1
broden-horizon <rlghd1698@naver.com>
2023-02-03 16:24:09 +0900

    feat(store): Store Controller
    fixes: #5

--- src/main/java/com/godsang/anytimedelivery/store/controller/StoreController.java
+++ src/main/java/com/godsang/anytimedelivery/store/controller/StoreController.java
@@ -1,13 +1,39 @@
 package com.godsang.anytimedelivery.store.controller;
 
+import com.godsang.anytimedelivery.common.dto.PageResponseDto;
+import com.godsang.anytimedelivery.store.dto.StoreDto;
+import com.godsang.anytimedelivery.store.entity.Store;
+import com.godsang.anytimedelivery.store.mapper.StoreMapper;
 import com.godsang.anytimedelivery.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * 고객의 Store 검색을 위한 Controller
+ */
 @RestController
 @RequestMapping("/customer/stores")
 @RequiredArgsConstructor
 public class StoreController {
-  private static StoreService storeService;
+  private final StoreService storeService;
+  private final StoreMapper storeMapper;
+
+  /**
+   * CategoryId를 가지고 Store를 검색한다.
+   * Pagination을 위해 page와 size 값도 입력 받는다.
+   */
+  @GetMapping
+  public ResponseEntity findByCategory(@RequestBody @Valid StoreDto.GetRequest getDto) {
+    Page<Store> stores = storeService.findByCategoryId(
+        getDto.getCategoryId(), PageRequest.of(getDto.getPage(), getDto.getSize()));
+    List<StoreDto.GetResponse> storeDtos = storeMapper.storeListToGetResponseDto(stores.getContent());
+    return new ResponseEntity(new PageResponseDto(storeDtos, stores), HttpStatus.OK);
+  }
 }
