630a7bf012ae534d3d72ca2f2453f220da7ab492
broden-horizon <rlghd1698@naver.com>
2023-02-03 16:14:41 +0900

    refactor(general): @Getter추가
    컨트롤러에서 응답할 때 객체를 직렬화해주는 Serializer는 해당 객채의 필드가 public이던가 아니면 getter메서드가 있어야함.

--- src/main/java/com/godsang/anytimedelivery/common/dto/PageInfo.java
+++ src/main/java/com/godsang/anytimedelivery/common/dto/PageInfo.java
@@ -1,8 +1,10 @@
 package com.godsang.anytimedelivery.common.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class PageInfo {
   private int page;
   private int size;
