513f3601a62d7389c9e3b3f77b47dba6ccc97c12
broden-horizon <rlghd1698@naver.com>
2023-02-06 20:16:15 +0900

    refactor(store): JpaConfig 삭제

--- src/main/java/com/godsang/anytimedelivery/store/controller/StoreController.java
+++ src/main/java/com/godsang/anytimedelivery/store/controller/StoreController.java
@@ -12,12 +12,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 /**
