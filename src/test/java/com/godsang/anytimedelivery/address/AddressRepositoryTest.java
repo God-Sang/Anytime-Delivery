b87c8d0bd00e83bb503ae9591055cbfb2e356f51
broden-horizon <rlghd1698@naver.com>
2023-02-06 20:29:57 +0900

    test(address): JpaMetamodelMappingContext 추가
    fixes: #7

--- src/test/java/com/godsang/anytimedelivery/address/AddressRepositoryTest.java
+++ src/test/java/com/godsang/anytimedelivery/address/AddressRepositoryTest.java
@@ -5,12 +5,15 @@ import com.godsang.anytimedelivery.address.repository.AddressRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataRedisTest
+@MockBean(JpaMetamodelMappingContext.class)
 public class AddressRepositoryTest {
   @Autowired
   private AddressRepository addressRepository;
