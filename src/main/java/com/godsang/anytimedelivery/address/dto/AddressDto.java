e9c892e1a5613d51e0e4e80578013cc22225a2d0
broden-horizon <rlghd1698@naver.com>
2023-02-10 16:29:31 +0900

    feat(address): addressDto 및 mapper
    related: #7

--- src/main/java/com/godsang/anytimedelivery/address/dto/AddressDto.java
+++ src/main/java/com/godsang/anytimedelivery/address/dto/AddressDto.java
@@ -1,32 +1,20 @@
 package com.godsang.anytimedelivery.address.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
-import java.util.List;
+import javax.validation.constraints.NotBlank;
 
 public class AddressDto {
   @Getter
-  public static class SearchResponseDto {
-    private Results results;
-
-    @Getter
-    public static class Results {
-      private Common common;
-      private List<Juso> juso;
-    }
-
-    @Getter
-    public static class Juso {
-      private String roadAddr; // 도로명 주소
-      private String jibunAddr; // 지번 주소
-      private String emdNm; // 읍면동
-    }
-
-    @Getter
-    public static class Common {
-      private String countPerPage;
-      private String totalPage;
-      private String currentPage;
-    }
+  @Setter
+  @Builder
+  public static class PostRequest {
+    @NotBlank
+    private String deliveryArea; // 시도명 + 시군구명 + 읍면동명, ex) "00시00구00동"
+    @NotBlank
+    private String address;
+    private String detailAddress;
   }
 }
---
+++ src/main/java/com/godsang/anytimedelivery/address/mapper/AddressMapper.java
@@ -0,0 +1,10 @@
+package com.godsang.anytimedelivery.address.mapper;
+
+import com.godsang.anytimedelivery.address.dto.AddressDto;
+import com.godsang.anytimedelivery.address.entity.Address;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AddressMapper {
+  Address postRequestDtoToAddress(AddressDto.PostRequest postRequest);
+}
