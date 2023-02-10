968d1d8603b408f7e80c3b26d8c9798b35b39267
broden-horizon <rlghd1698@naver.com>
2023-02-10 16:28:32 +0900

    feat(user): 유저 찾지 못하는 exception 추가
    related: #7

--- src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionCode.java
+++ src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionCode.java
@@ -6,11 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ExceptionCode {
+  // Category
   CATEGORY_ALREADY_EXISTS(409, "Category already exists."),
   CATEGORY_NOT_FOUND(404, "No Category found"),
+
+  // User
   EMAIL_ALREADY_EXISTS(409, "Email already exists."),
   PHONE_NUMBER_ALREADY_EXISTS(409, "Phone number already exists."),
-  NICKNAME_ALREADY_EXISTS(409, "Nickname already exists.");
+  NICKNAME_ALREADY_EXISTS(409, "Nickname already exists."),
+  USER_NOT_FOUND(404, "User does not exits");
 
   private final int code;
   private final String description;
