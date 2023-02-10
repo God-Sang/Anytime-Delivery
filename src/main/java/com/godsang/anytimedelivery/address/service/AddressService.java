7b4a2c281a35447f938b0f00d4b52d716c0b258c
broden-horizon <rlghd1698@naver.com>
2023-02-10 16:25:23 +0900

    feat(address): 주소 저장 기능 및 User Service에 findUser 추가
    fixes: #7

--- src/main/java/com/godsang/anytimedelivery/address/service/AddressService.java
+++ src/main/java/com/godsang/anytimedelivery/address/service/AddressService.java
@@ -1,47 +1,20 @@
 package com.godsang.anytimedelivery.address.service;
 
-import com.godsang.anytimedelivery.address.dto.AddressDto;
-import com.godsang.anytimedelivery.address.repository.AddressRepository;
+import com.godsang.anytimedelivery.address.entity.Address;
+import com.godsang.anytimedelivery.user.entity.User;
+import com.godsang.anytimedelivery.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AddressService {
-  private static AddressRepository addressRepository;
-  private final String URI = "https://business.juso.go.kr/addrlink/addrLinkApi.do";
-  @Value("${address.search.key}")
-  private String key;
+  private final UserService userService;
 
-  public AddressDto.SearchResponseDto searchAddress(String keyword, int page, int size) {
-    //인코딩 기능을 추가한 URI 빌더
-    DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(URI);
-    factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.URI_COMPONENT);
-
-    //WebClient 인스턴스 생성
-    WebClient client = WebClient.builder()
-        .uriBuilderFactory(factory)
-        .baseUrl(URI)
-        .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-        .build();
-
-    AddressDto.SearchResponseDto response = client.get()
-        .uri(uriBuilder -> uriBuilder
-            .queryParam("confmKey", key)
-            .queryParam("currentPage", page)
-            .queryParam("countPerPage", size)
-            .queryParam("keyword", keyword)
-            .queryParam("resultType", "json")
-            .build()
-        )
-        .retrieve()
-        .bodyToMono(AddressDto.SearchResponseDto.class)
-        .block();
-    return response;
+  @Transactional
+  public void saveAddress(Long userId, Address address) {
+    User user = userService.findUser(userId);
+    user.setAddress(address);
   }
 }
