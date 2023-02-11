c59c8428206945b8efcf0946230529b6b84c2fc0
NAM GIBEOM <rlqja0523@naver.com>
2023-02-12 00:06:07 +0900

    feat: 예외 코드 추가

--- src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionCode.java
+++ src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionCode.java
@@ -10,7 +10,10 @@ public enum ExceptionCode {
   CATEGORY_NOT_FOUND(404, "No Category found"),
   EMAIL_ALREADY_EXISTS(409, "Email already exists."),
   PHONE_NUMBER_ALREADY_EXISTS(409, "Phone number already exists."),
-  NICKNAME_ALREADY_EXISTS(409, "Nickname already exists.");
+  NICKNAME_ALREADY_EXISTS(409, "Nickname already exists."),
+  REGISTRATION_NUMBER_ALREADY_EXISTS(409, "Registration number already exists."),
+  TEL_ALREADY_EXISTS(409, "Tel already exists."),
+  ADDRESS_ALREADY_EXISTS(409, "Address already exists.");
 
   private final int code;
   private final String description;
