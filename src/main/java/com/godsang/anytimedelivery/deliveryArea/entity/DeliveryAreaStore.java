6ad53e1562a878df2dc4b51f1d031c6025d8f96b
NAM GIBEOM <rlqja0523@naver.com>
2023-02-12 03:15:13 +0900

    refactor(store): setter 제거 #15

--- src/main/java/com/godsang/anytimedelivery/deliveryArea/entity/DeliveryAreaStore.java
+++ src/main/java/com/godsang/anytimedelivery/deliveryArea/entity/DeliveryAreaStore.java
@@ -1,16 +1,13 @@
 package com.godsang.anytimedelivery.deliveryArea.entity;
 
-import com.godsang.anytimedelivery.deliveryArea.entity.DeliveryArea;
 import com.godsang.anytimedelivery.store.entity.Store;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class DeliveryAreaStore {
