1be0a63b3162bc2aa5746aabfe24662e130afa0f
broden-horizon <rlghd1698@naver.com>
2023-02-13 14:48:57 +0900

    merge

--- src/main/java/com/godsang/anytimedelivery/user/entity/User.java
+++ src/main/java/com/godsang/anytimedelivery/user/entity/User.java
@@ -8,13 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
