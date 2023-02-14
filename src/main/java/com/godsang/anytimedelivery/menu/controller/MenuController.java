5c655c1580e6f56a33bce881ee17c5915c6b67f8
NAM GIBEOM <rlqja0523@naver.com>
2023-02-14 11:56:13 +0900

    feat(menu): controller 생성, 구현 미완료 #16

---
+++ src/main/java/com/godsang/anytimedelivery/menu/controller/MenuController.java
@@ -0,0 +1,45 @@
+package com.godsang.anytimedelivery.menu.controller;
+
+import com.godsang.anytimedelivery.menu.dto.GroupDto;
+import com.godsang.anytimedelivery.menu.dto.MenuDto;
+import com.godsang.anytimedelivery.menu.entity.Group;
+import com.godsang.anytimedelivery.menu.entity.Menu;
+import com.godsang.anytimedelivery.menu.entity.Option;
+import com.godsang.anytimedelivery.menu.mapper.MenuMapper;
+import com.godsang.anytimedelivery.menu.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@RestController
+@RequestMapping("/owner/stores/{store-id}")
+@RequiredArgsConstructor
+@Validated
+public class MenuController {
+  private final MenuService menuService;
+  private final MenuMapper menuMapper;
+
+  /**
+   * 가게의 메뉴를 등록한다.
+   */
+  @PostMapping
+  public ResponseEntity postMenu(@PathVariable("store-id") @Positive long storeId,
+                                 @RequestBody @Valid MenuDto menuDto) {
+    Menu menu = menuMapper.menuDtoToMenu(menuDto);
+    for (GroupDto groupDto : menuDto.getGroups()) {
+      Group group = menuMapper.groupDtoToGroup(groupDto);
+      List<Option> options = menuMapper.optionDtosToOptions(groupDto.getOptions());
+      menu = menuService.createMenu(storeId, menu, group, options);
+    }
+    return null;
+  }
+}
