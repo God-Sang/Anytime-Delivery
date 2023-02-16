6abe0b101bf7935f22feed92e6db82ea14028a16
NAM GIBEOM <rlqja0523@naver.com>
2023-02-16 16:39:15 +0900

    test(menu): 메뉴 조회 테스트 #16

--- src/test/java/com/godsang/anytimedelivery/menu/MenuIntegrationTest.java
+++ src/test/java/com/godsang/anytimedelivery/menu/MenuIntegrationTest.java
@@ -8,14 +8,18 @@ import com.godsang.anytimedelivery.menu.entity.Menu;
 import com.godsang.anytimedelivery.menu.repository.MenuRepository;
 import com.godsang.anytimedelivery.store.entity.Store;
 import com.godsang.anytimedelivery.store.repository.StoreRepository;
+import com.godsang.anytimedelivery.store.service.StoreService;
 import com.godsang.anytimedelivery.user.entity.Role;
 import com.godsang.anytimedelivery.user.entity.User;
 import com.godsang.anytimedelivery.user.repository.UserRepository;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +28,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,6 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Transactional
 @WithMockCustomUser(role = Role.ROLE_OWNER)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MenuIntegrationTest {
   @Autowired
   private MockMvc mockMvc;
@@ -44,6 +50,8 @@ public class MenuIntegrationTest {
   private StoreRepository storeRepository;
   @Autowired
   private MenuRepository menuRepository;
+  @Autowired
+  private StoreService storeService;
   private Long storeId;
 
   @BeforeAll
@@ -63,6 +71,7 @@ public class MenuIntegrationTest {
 
   @Test
   @DisplayName("메뉴등록 성공")
+  @Order(100)
   void createStoreTest() throws Exception {
     // given
     MenuDto.Post post = StubData.MockMenuPost.getMenuDto();
@@ -87,5 +96,24 @@ public class MenuIntegrationTest {
 
     String optionName = menu.getGroups().get(0).getOptions().get(0).getName();
     assertThat(optionName).isEqualTo(post.getGroups().get(0).getOptions().get(0).getName());
+  }
+
+  @Test
+  @DisplayName("가게의 메뉴 조회")
+  @WithMockCustomUser(role = Role.ROLE_CUSTOMER)
+  @Order(200)
+  void findMenusTest() throws Exception {
+    // given
+    Store store = storeService.findStoreById(storeId);
+    Menu menu = StubData.MockMenu.getMockMenuEntity();
+    menu.setStore(store);
+    menuRepository.save(menu);
+
+    // when
+    mockMvc.perform(
+            get("/categories/{category-id}/stores/{store-id}/menu", 1L, storeId)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(jsonPath("$.data[0].menuId").value(2));
   }
 }
