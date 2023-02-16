e12d97c20a3658cfa719491733395ff069d27e0e
NAM GIBEOM <rlqja0523@naver.com>
2023-02-16 16:38:50 +0900

    fix(menu): 테스트 통과되도록 다시 매핑 #16

--- src/test/java/com/godsang/anytimedelivery/menu/MenuRepositoryTest.java
+++ src/test/java/com/godsang/anytimedelivery/menu/MenuRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.godsang.anytimedelivery.menu;
 
 import com.godsang.anytimedelivery.helper.StubData;
+import com.godsang.anytimedelivery.menu.entity.ChoiceType;
 import com.godsang.anytimedelivery.menu.entity.Group;
 import com.godsang.anytimedelivery.menu.entity.Menu;
 import com.godsang.anytimedelivery.menu.entity.Option;
@@ -14,7 +15,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +27,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class MenuRepositoryTest {
   private final String registrationNumber = "123-12-12345";
   private final String title = "치즈볼 택 1";
-  private final String choiceType = "check";
+  private final ChoiceType choiceType = ChoiceType.CHECK;
   private final String[] optionName = {"기본 치즈볼", "초코 치즈볼", "크림 치즈볼"};
   @Autowired
   private MenuRepository menuRepository;
@@ -39,17 +39,23 @@ public class MenuRepositoryTest {
     Store store = StubData.MockStore.getMockEntity(1L, registrationNumber, "애니타임 치킨", "02-123-1234", "서울특별시 강남구 강남대로 123");
     storeRepository.save(store);
 
-    Menu menu = StubData.MockMenu.getMockMenuEntity("푸다닥 치킨", 30000);
     Group group = StubData.MockMenu.getMockGroupEntity(title, choiceType);
     List<Option> options = new ArrayList<>();
     for (int i = 0; i < optionName.length; i++) {
-      options.add(StubData.MockMenu.getOption(optionName[i], 5000, group));
-    }
-    group.setMenu(menu);
-    menu.setStore(store);
+      Option option = StubData.MockMenu.getOption(optionName[i], 5000);
+      option.setGroup(group);
+      options.add(option);
 
-    menu.addGroup(group);
+    }
+    Menu menu = StubData.MockMenu.getMockMenuEntity("푸다닥 치킨", 30000);
+    List<Group> groups = new ArrayList<>();
+    group.setMenu(menu);
     group.setOptions(options);
+    groups.add(group);
+
+    menu.setStore(store);
+    menu.setGroups(groups);
+
     menuRepository.save(menu);
   }
 
