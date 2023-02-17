86cca12354965e8bf722174de9e605b5d4641dcd
broden-horizon <rlghd1698@naver.com>
2023-02-18 01:22:41 +0900

    test(order): 유저 아이디로 DeliveryArea 조회 기능 테스트
    User만 조회한 후 그래프 탐색하면, 불필요한 조회를 많이 하게 됩니다.
    그리고, JPQL 객체지향쿼리로 작성할 수 없어서 Native를 사용했습니다.
    
    related to: #40

--- src/test/java/com/godsang/anytimedelivery/address/AddressRepositoryTest.java
+++ src/test/java/com/godsang/anytimedelivery/address/AddressRepositoryTest.java
@@ -4,54 +4,62 @@ import com.godsang.anytimedelivery.address.entity.Address;
 import com.godsang.anytimedelivery.deliveryArea.entity.DeliveryArea;
 import com.godsang.anytimedelivery.deliveryArea.repository.DeliveryAreaRepository;
 import com.godsang.anytimedelivery.helper.stub.StubData;
+import com.godsang.anytimedelivery.user.entity.Role;
 import com.godsang.anytimedelivery.user.entity.User;
 import com.godsang.anytimedelivery.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AddressRepositoryTest {
   @Autowired
   private UserRepository userRepository;
   @Autowired
   private DeliveryAreaRepository deliveryAreaRepository;
-
-  @Test
-  @Transactional
-  @DisplayName("유저 - 주소 - 배달지역 매핑 테스트")
-  void saveAndRetrieveTest() {
-    //given
-    User user = userRepository.save(StubData.MockUser.getMockEntity(
-        2L, "email", "010", "nickName"));
-
-    Long userId = 1L;
-    String addr = "서울시 행복구 행복동";
+  private Long userId;
+  private String addr = "서울시 행복구 행복동";
+  @BeforeAll
+  void init() {
+    User user = userRepository.save(StubData.MockUser.getMockEntity(Role.ROLE_CUSTOMER));
+    DeliveryArea deliveryArea = deliveryAreaRepository.save(new DeliveryArea(addr));
     Address address = Address.builder()
-        .address(addr)
+        .address("서울시 행복구 행복동")
         .detailAddress("10층 페이팀")
         .build();
-
-    DeliveryArea deliveryArea = deliveryAreaRepository.save(new DeliveryArea("서울시 행복구 행복동"));
-
-    //when
-    User foundUser = userRepository.findById(user.getUserId()).get();
     address.setDeliveryArea(deliveryArea);
-    foundUser.setAddress(address);
+    user.setAddress(address);
+    userRepository.save(user);
+    userId = user.getUserId();
+  }
 
-    userRepository.save(foundUser);
-
+  @Test
+  @DisplayName("유저 - 주소 - 배달지역 매핑 테스트")
+  void saveAndRetrieveTest() {
     User retrievedUser = userRepository.findById(userId).get();
     Address retrievedAddress = retrievedUser.getAddress();
     String retrievedDeliveryArea = retrievedAddress.getDeliveryArea().getJuso();
 
     // then
+    assertThat(retrievedAddress.getAddressId()).isNotNull();
     assertThat(retrievedAddress.getAddress()).isEqualTo(addr);
     assertThat(retrievedDeliveryArea).isEqualTo(addr);
+  }
+  @Test
+  void findUserDeliveryAreaTest() {
+    DeliveryArea deliveryArea = deliveryAreaRepository.findUserDeliveryArea(userId).get();
+
+    assertThat(deliveryArea).isNotNull();
   }
 
 
