dc821a5822f0e362431d5099cc2153671953227e
broden-horizon <rlghd1698@naver.com>
2023-02-18 01:12:02 +0900

    refactor(order): 컬럼 명 컨벤션 지키기 위해 명시
    related to: #40

--- src/main/java/com/godsang/anytimedelivery/address/entity/Address.java
+++ src/main/java/com/godsang/anytimedelivery/address/entity/Address.java
@@ -6,7 +6,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Getter
 @Builder
@@ -21,6 +28,7 @@ public class Address {
   private String address;
   private String detailAddress;
   @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "delivery_area_id")
   private DeliveryArea deliveryArea;
 
   public void setDeliveryArea(DeliveryArea deliveryArea) {
