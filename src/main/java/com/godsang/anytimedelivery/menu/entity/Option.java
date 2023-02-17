b88df6f35214fb4c2f26f0030e55bf1a400f4988
broden-horizon <rlghd1698@naver.com>
2023-02-18 01:15:48 +0900

    refactor(order): OrderDto -> Order 변환시 Id만 가지고 있는 객체로 매핑, User의 Cascade설정 변경.
    영속성 컨텍스트 안에서 수정(추가)이 되는 경우를 반영하려면 Merge를 추가해야함.
    
    related to: #40

--- src/main/java/com/godsang/anytimedelivery/menu/entity/Option.java
+++ src/main/java/com/godsang/anytimedelivery/menu/entity/Option.java
@@ -31,6 +31,10 @@ public class Option {
   @JoinColumn(name = "group_id")
   private Group group;
 
+  public Option(Long optionId) {
+    this.optionId = optionId;
+  }
+
   @Builder
   private Option(String name, int price) {
     this.name = name;
