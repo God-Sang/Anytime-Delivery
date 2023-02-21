99291e77283d6b2ae708851d9ebbceeca7c7d45e
broden-horizon <rlghd1698@naver.com>
2023-02-21 17:43:59 +0900

    refactor(order): 불필요 annotation 삭제
    fixes: #8, #9

--- src/test/java/com/godsang/anytimedelivery/order/OrderRepositoryTest.java
+++ src/test/java/com/godsang/anytimedelivery/order/OrderRepositoryTest.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -30,7 +29,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @DataJpaTest
-@Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class OrderRepositoryTest {
