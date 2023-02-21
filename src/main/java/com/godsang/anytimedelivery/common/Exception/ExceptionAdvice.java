d6a1236a9e64d367d16f611a368b4cb752f50374
broden-horizon <rlghd1698@naver.com>
2023-02-21 19:18:07 +0900

    refactor(order): Enum Convert Exception Handler
    Spring은 자동으로 Enum으로 변환이 안되면 예외를 뱉음 -> validate
    즉, Client에게 적절한 메시지를 보내주는 것이 필요. -> ExceptionHandler를 작성.
    
    fixes: #8, #9

--- src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionAdvice.java
+++ src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionAdvice.java
@@ -1,10 +1,12 @@
 package com.godsang.anytimedelivery.common.Exception;
 
+import com.godsang.anytimedelivery.order.entity.OrderStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ConstraintViolationException;
 
@@ -23,5 +25,12 @@ public class ExceptionAdvice {
   @ExceptionHandler
   public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
     return new ResponseEntity(ErrorResponse.of(e), HttpStatus.valueOf(e.getExceptionCode().getCode()));
+  }
+  @ExceptionHandler
+  public ResponseEntity handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    if (e.getRequiredType().equals(OrderStatus.class)) {
+      return new ResponseEntity(ErrorResponse.of(e), HttpStatus.BAD_REQUEST);
+    }
+    throw e;
   }
 }
--- src/test/java/com/godsang/anytimedelivery/order/OrderControllerForOwnerTest.java
+++ src/test/java/com/godsang/anytimedelivery/order/OrderControllerForOwnerTest.java
@@ -4,13 +4,10 @@ import com.godsang.anytimedelivery.helper.annotation.WithMockCustomUser;
 import com.godsang.anytimedelivery.helper.stub.MockDto;
 import com.godsang.anytimedelivery.order.controller.OrderForOwnerController;
 import com.godsang.anytimedelivery.order.dto.OrderDto;
-import com.godsang.anytimedelivery.order.entity.OrderStatus;
 import com.godsang.anytimedelivery.order.mapper.OrderMapper;
 import com.godsang.anytimedelivery.order.service.OrderService;
-import com.godsang.anytimedelivery.store.controller.StoreForCustomerController;
 import com.godsang.anytimedelivery.user.entity.Role;
 import com.google.gson.Gson;
-import jdk.jfr.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +23,7 @@ import org.springframework.util.MultiValueMap;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -68,5 +66,29 @@ public class OrderControllerForOwnerTest {
         // then
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.data[0].address").value(responses.get(0).getAddress()));
+  }
+
+  @Test
+  @DisplayName("주문 리스트 조회 예외 : Enum에 없는 값을 입력한 경우")
+  void retrieveOrdersExceptionTest() throws Exception {
+    // given
+    Long storeId = 1L;
+    MultiValueMap<String, String> queries = new LinkedMultiValueMap<>();
+    queries.add("order-state", "WAI");
+    queries.add("page", String.valueOf(1));
+    queries.add("size", String.valueOf(5));
+    List<OrderDto.ResponseForList> responses = MockDto.OrderResponse.getList();
+    given(orderMapper.ordersToResponses(any())).willReturn(responses);
+
+    // when
+    mockMvc.perform(
+            get("/owner/stores/{store-id}/orders", storeId)
+                .queryParams(queries)
+                .accept(MediaType.APPLICATION_JSON)
+
+        )
+        // then
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.message").value(startsWith("A value")));
   }
 }
