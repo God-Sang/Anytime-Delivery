2950baa3c66e9599ff94469836e4de2ef184ce85
broden-horizon <rlghd1698@naver.com>
2023-02-21 12:57:04 +0900

    feat(order): Order 리스트 조회와, Order 상세 조회 기능.
    Order리스트 조회할 떄는 , 거의 1:1 관계에 있는 Order-User, 1:1관계인 User-Address를 fetch join을 통해 한번에 조회.
    Order 상세 조회할 떄는, Batch를 이용해 조회
    
    fixes: #8, #9

--- src/main/java/com/godsang/anytimedelivery/order/service/OrderService.java
+++ src/main/java/com/godsang/anytimedelivery/order/service/OrderService.java
@@ -6,13 +6,16 @@ import com.godsang.anytimedelivery.deliveryArea.entity.DeliveryArea;
 import com.godsang.anytimedelivery.deliveryArea.entity.DeliveryAreaStore;
 import com.godsang.anytimedelivery.deliveryArea.service.DeliveryAreaService;
 import com.godsang.anytimedelivery.order.entity.Order;
+import com.godsang.anytimedelivery.order.entity.OrderStatus;
 import com.godsang.anytimedelivery.order.repository.OrderRepository;
 import com.godsang.anytimedelivery.store.entity.Store;
 import com.godsang.anytimedelivery.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +26,12 @@ public class OrderService {
   private final DeliveryAreaService deliveryAreaService;
   private final PayService payService;
 
+  /**
+   * validate and save an order as OrderStatus.WAIT.
+   *
+   * @param order
+   * @return Order
+   */
   @Transactional
   public Order request(Order order) {
     Store store = storeService.findStoreById(order.getStore().getStoreId());
@@ -31,12 +40,61 @@ public class OrderService {
     return orderRepository.save(order);
   }
 
+  /**
+   * check if it's possible to cancel and cancel the order.
+   *
+   * @param orderId
+   * @param userId
+   */
+
   @Transactional
   public void cancel(Long orderId, Long userId) {
     Order order = findVerifiedOrder(orderId);
     checkIfUsersOrder(order, userId);
     order.changeStatesToCanceled();
     orderRepository.save(order);
+  }
+
+  /**
+   * Retrieve orders after validate if its store is owned by user.
+   *
+   * @param storeId
+   * @param userId
+   * @param orderStatus
+   * @param pageable
+   * @return List<Order>
+   */
+  @Transactional(readOnly = true)
+  public List<Order> retrieveOrders(Long storeId, Long userId, OrderStatus orderStatus, Pageable pageable) {
+    Store store = verifyStoreOwner(storeId, userId);
+    List<Order> orders = orderRepository.findAllByStoreAndStatus(store, orderStatus, pageable);
+    return orders;
+  }
+
+  /**
+   * retrieve a specific order.
+   *
+   * @param storeId
+   * @param userId
+   * @param orderId
+   * @return
+   */
+  @Transactional(readOnly = true)
+  public Order retrieveOrder(Long storeId, Long userId, Long orderId) {
+    verifyStoreOwner(storeId, userId);
+    Order order = findVerifiedOrder(orderId);
+    order.getUser().getAddress();
+    return order;
+  }
+
+  @Transactional(readOnly = true)
+  private Store verifyStoreOwner(Long storeId, Long userId) {
+    Store store = storeService.findStoreById(storeId);
+    Long ownerId = store.getUser().getUserId();
+    if (!ownerId.equals(userId)) {
+      throw new BusinessLogicException(ExceptionCode.STORE_NOT_YOURS);
+    }
+    return store;
   }
 
   private void checkIfUsersOrder(Order order, Long userId) {
@@ -60,7 +118,7 @@ public class OrderService {
   }
 
   @Transactional(readOnly = true)
-  public void verifyPossibleArea(Store store, Long userId) {
+  private void verifyPossibleArea(Store store, Long userId) {
     boolean isPossible = false;
     DeliveryArea userDeliveryArea = deliveryAreaService.findUserDeliveryArea(userId);
     for (DeliveryAreaStore deliveryAreaStore : store.getDeliveryAreaStores()) {
