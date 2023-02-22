b0b9ea2e77aa61350ad9dae43f8e64ca2ab5860a
broden-horizon <rlghd1698@naver.com>
2023-02-23 00:56:13 +0900

    test(order): 주문 상태 변경 및 취소 테스트

--- src/test/java/com/godsang/anytimedelivery/order/OrderControllerForOwnerTest.java
+++ src/test/java/com/godsang/anytimedelivery/order/OrderControllerForOwnerTest.java
@@ -4,12 +4,15 @@ import com.godsang.anytimedelivery.helper.annotation.WithMockCustomUser;
 import com.godsang.anytimedelivery.helper.stub.MockDto;
 import com.godsang.anytimedelivery.order.controller.OrderForOwnerController;
 import com.godsang.anytimedelivery.order.dto.OrderDto;
+import com.godsang.anytimedelivery.order.entity.OrderStatus;
 import com.godsang.anytimedelivery.order.mapper.OrderMapper;
 import com.godsang.anytimedelivery.order.service.OrderService;
 import com.godsang.anytimedelivery.user.entity.Role;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,6 +30,7 @@ import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -90,5 +94,83 @@ public class OrderControllerForOwnerTest {
         // then
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.message").value(startsWith("A value")));
+  }
+
+  @Test
+  @DisplayName("주문 상태 변경 조회")
+  void changeOrderStateTest() throws Exception {
+    // given
+    Long storeId = 1L;
+    Long orderId = 1L;
+    OrderDto.Patch patch = MockDto.OrderPatch.get(OrderStatus.ACCEPTED);
+    String content = gson.toJson(patch);
+    // when
+    mockMvc.perform(
+            patch("/owner/stores/{store-id}/orders/{order_id}", storeId, orderId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content)
+        )
+        // then
+        .andExpect(status().isOk());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"WAIT", "CANCELED"})
+  @DisplayName("주문 상태 변경 조회 예외 : WAIT / CANCELED 로 변경하는 경우")
+  void changeOrderStateExceptionTest(String orderStatus) throws Exception {
+    // given
+    Long storeId = 1L;
+    Long orderId = 1L;
+    OrderDto.Patch patch = MockDto.OrderPatch.get(OrderStatus.valueOf(orderStatus));
+    String content = gson.toJson(patch);
+    // when
+    mockMvc.perform(
+            patch("/owner/stores/{store-id}/orders/{order_id}", storeId, orderId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content)
+        )
+        // then
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @DisplayName("주문 취소")
+  void cancelOrderTest() throws Exception {
+    // given
+    Long storeId = 1L;
+    Long orderId = 1L;
+    OrderDto.PatchCancel patchCancel = MockDto.OrderPatchCancel.get("재료 소진");
+    String content = gson.toJson(patchCancel);
+    // when
+    mockMvc.perform(
+            patch("/owner/stores/{store-id}/orders/{order_id}/cancel", storeId, orderId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content)
+        )
+        // then
+        .andExpect(status().isOk());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {" ", ""})
+  @DisplayName("주문 취소 예외 : 사유가 blank인 경우")
+  void cancelOrderExceptionTest(String reason) throws Exception {
+    // given
+    Long storeId = 1L;
+    Long orderId = 1L;
+    OrderDto.PatchCancel patchCancel = MockDto.OrderPatchCancel.get(reason);
+    String content = gson.toJson(patchCancel);
+    // when
+    mockMvc.perform(
+            patch("/owner/stores/{store-id}/orders/{order_id}/cancel", storeId, orderId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content)
+        )
+        // then
+        .andExpect(status().isBadRequest());
   }
 }
