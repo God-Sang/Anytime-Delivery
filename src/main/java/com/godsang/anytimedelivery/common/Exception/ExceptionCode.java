49afe7fa97b49d2359489161a7a5325312e65ecd
broden-horizon <rlghd1698@naver.com>
2023-02-23 00:32:47 +0900

    refactor(order): 예외 코드 추가

--- src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionCode.java
+++ src/main/java/com/godsang/anytimedelivery/common/Exception/ExceptionCode.java
@@ -35,7 +35,9 @@ public enum ExceptionCode {
   NOT_IN_DELIVERY_AREA(409, "Your address is not in the store's delivery area."),
   INVALID_ORDER_STATES_CHANGE(403, "Invalid change of an order state"),
   ORDER_NOT_EXIST(404, "The order does not exist."),
-  ORDER_NOT_YOURS(403, "This order is not yours");
+  ORDER_NOT_YOURS(403, "This order is not yours"),
+  UNABLE_TO_CHANGE_TO_WAIT(403, "An order state can not be changed to WAIT"),
+  ORDER_NOT_BELONG_TO_STORE(403, "This order does not belong to your store");
 
   private final int code;
   private final String description;
